6369315b402d3c6a9a37800e534b9986616d8f2d
kiran <kiranugale239@gmail.com>
2024-04-20 22:26:21 +0530

    add login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,8 @@
-import { SingUpHandler } from "../controller/user.controller.js";
+import { SingUpHandler, loginHandler } from "../controller/user.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/singup").post(SingUpHandler);
-
+router.route("/login").post(loginHandler);
 export default router;
