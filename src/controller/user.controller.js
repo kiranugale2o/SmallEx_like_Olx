31251b7784f877b8983a293cca725261bec11e85
kiran <kiranugale239@gmail.com>
2024-04-20 22:24:29 +0530

    user login api done

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,13 +1,44 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import userValidation from "../validation/userValidation.js";
+import bcrypt from "bcryptjs";
+
+//singup user
 export const SingUpHandler = async (req, res) => {
   try {
     await userValidation.validateAsync(req.body);
+    const isExit = await User.findOne({ email: req.body.email });
+    if (isExit) {
+      return res.status(400).json({ message: "this email is already use" });
+    }
     const user = await User(req.body);
     await user.save();
     if (user) {
       res.status(200).json({ message: "successfully register" });
+    }
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+};
+
+//login user
+
+export const loginHandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        const token = await user.generateAuthToken();
+        return res
+          .status(200)
+          .json({ message: "login successfull", token: token });
+      } else {
+        return res.status(400).json({ message: "invalid Password" });
+      }
+    } else {
+      return res.status(400).json({ message: "invalid Email" });
     }
   } catch (error) {
     return res.status(400).json({ status: false, message: error.message });
