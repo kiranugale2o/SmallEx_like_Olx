94f6a0119ca4f912a4c66fd2639b7f76f885671c
kiran <kiranugale239@gmail.com>
2024-04-20 22:39:44 +0530

    add update profile route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,13 @@
-import { SingUpHandler, loginHandler } from "../controller/user.controller.js";
+import {
+  SingUpHandler,
+  loginHandler,
+  updateProfile,
+} from "../controller/user.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/singup").post(SingUpHandler);
 router.route("/login").post(loginHandler);
+router.route("/update/:id").put(updateProfile);
 export default router;
