6e013b136a3a8def4a94e0799fd55288a11428da
kiran <kiranugale239@gmail.com>
2024-04-20 22:53:30 +0530

    add get yourproduct route

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,6 +4,7 @@ import {
   createProduct,
   deleteProduct,
   singleProduct,
+  yourProducts,
 } from "../controller/product.controller.js";
 
 const router = Router();
@@ -12,4 +13,5 @@ router.route("/create").post(createProduct);
 router.route("/allproducts").get(allProducts);
 router.route("/singleproduct/:id").get(singleProduct);
 router.route("/deleteproduct/:id").delete(deleteProduct);
+router.route("/yourproducts/:id").get(yourProducts);
 export default router;
