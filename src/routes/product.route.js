4121f9e0387e6f3834c6a7c10a1acfb652e4b108
kiran <kiranugale239@gmail.com>
2024-04-20 21:57:52 +0530

    add deleteproduct route in product routes

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   allProducts,
   createProduct,
+  deleteProduct,
   singleProduct,
 } from "../controller/product.controller.js";
 
@@ -10,4 +11,5 @@ const router = Router();
 router.route("/create").post(createProduct);
 router.route("/allproducts").get(allProducts);
 router.route("/singleproduct/:id").get(singleProduct);
+router.route("/deleteproduct/:id").delete(deleteProduct);
 export default router;
