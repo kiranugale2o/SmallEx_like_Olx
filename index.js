d21578706ab5393edbe6c55a96c02ed97a1038c9
kiran <kiranugale239@gmail.com>
2024-04-23 09:51:12 +0530

    make some changes

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 import dbconnection from "./src/Config/dbconnection.js";
 import userRoute from "./src/routes/user.route.js";
 import productRoute from "./src/routes/product.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 dbconnection();
+let originAllow = {
+  origin: "http://localhost:5173",
+  methods: "PUT,GET,POST,DELETE,PATCH,HEAD",
+  credentials: true,
+};
+
 const app = express();
 app.use(express.json());
+app.use(cors(originAllow));
 app.use(express.urlencoded({ extended: false }));
 app.use("/user", userRoute);
 app.use("/product", productRoute);
